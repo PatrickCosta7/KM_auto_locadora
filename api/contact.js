5896f542aa6b7d70e9579e0dacb0ef4761227903
PatrickCosta7 <pl.costa@hotmail.com>
2025-06-27 14:51:04 -0300

    bugfix: destination email corrected

--- api/contact.js
+++ api/contact.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
   try {
     await resend.emails.send({
       from: 'onboarding@resend.dev',
-      to: 'costaetcorreia27@gmail.com',
+      to: 'pl.costa@hotmail.com',
       subject: 'New Form Submission',
       html: `<p><strong>Nome:</strong> ${name}</p>
              <p><strong>Telefone:</strong> ${phone}</p>
